17fb43bdc15a9701c2fb9bc222065880b3159a35
mollie gold <molliegold@mollies-Air.wireless.newschool.edu>
2017-04-13 19:00:05 -0400

    final pages plus some jquery

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,23 @@
-// $( function() {
-//     $( "#speed" ).selectmenu();
- 
-//     $( "#files" ).selectmenu();
- 
-//     $( "#number" )
-//       .selectmenu()
-//       .selectmenu( "menuWidget" )
-//         .addClass( "overflow" );
- 
-//     $( "#salutation" ).selectmenu();
-// });
+$(document).ready(function() {
+  $('.no-fb-text').click(function(){
+    $('.no-facebook').css('display','inherit');
+  });
+
+  $('.close').click(function(){
+  	$('.no-facebook').css('display','none');
+  });
+
+  $('.review1').click(function(){
+    $('.james').css('display','inherit');
+  });
+
+  $('.close').click(function(){
+  	$('.james').css('display','none');
+  });
+
+  // $('.price').click(function(){
+  // 	$('span').css('background-color','#8EBDF3');
+  // });
+
+});
+
